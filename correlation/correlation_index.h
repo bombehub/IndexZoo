11c1d07ace7a5a83664e3132e69a2e9edd6e0468
yingjun <wu.yj0616@gmail.com>
2018-09-10 19:48:36 -0700

    add interpolation correlation index

--- correlation/correlation_index.h
+++ correlation/correlation_index.h
@@ -41,30 +41,259 @@ private:
 
 };
 
-struct AttributePair {
-  AttributePair() : target_(0), host_(0) {}
-  AttributePair(const uint64_t target, const uint64_t host) : target_(target), host_(host) {}
-
-  uint64_t target_;
-  uint64_t host_;
-};
-
-static bool compare_func(AttributePair &lhs, AttributePair &rhs) {
-  return lhs.target_ < rhs.target_;
-}
-
-
 class InterpolationCorrelationIndex : public BaseCorrelationIndex {
-public:
-  InterpolationCorrelationIndex() : container_(nullptr), size_(0) {}
-  virtual ~InterpolationCorrelationIndex() {
-    if (container_ != nullptr) {
-      delete[] container_;
-      container_ = nullptr;
-    }
+
+  struct AttributePair {
+    AttributePair() : target_(0), host_(0) {}
+    AttributePair(const uint64_t target, const uint64_t host) : target_(target), host_(host) {}
+
+    uint64_t target_;
+    uint64_t host_;
+  };
+
+  static bool compare_func(AttributePair &lhs, AttributePair &rhs) {
+    return lhs.target_ < rhs.target_;
   }
 
+  struct Stats {
+
+    Stats() : 
+      is_first_match_(true), 
+      find_op_count_(0), 
+      find_op_profile_count_(0), 
+      find_op_guess_distance_(0) {}
+
+    void increment_find_op_counter() {
+      find_op_count_++;
+      is_first_match_ = true;
+    }
+
+    void measure_find_op_guess_distance(const int64_t guess_pos, const int64_t find_pos) {
+      // record guess distance
+      if (find_op_count_ % 1000 == 0) {
+        if (is_first_match_ == true) {
+          find_op_guess_distance_ += std::abs(guess_pos - find_pos);
+          find_op_profile_count_ += 1;
+          is_first_match_ = false;
+        }
+      }
+    }
+
+    bool is_first_match_;
+    uint64_t find_op_count_;
+    uint64_t find_op_profile_count_;
+    uint64_t find_op_guess_distance_;
+  };
+
+public:
+  InterpolationCorrelationIndex(const size_t num_segments = 1) {
+
+    ASSERT(num_segments >= 1, "must have at least one segment");
+
+    num_segments_ = num_segments;
+    segment_key_boundaries_ = new uint64_t[num_segments_ + 1];
+    memset(segment_key_boundaries_, 0, sizeof(uint64_t) * (num_segments_ + 1));
+
+    segment_offset_boundaries_ = new size_t[num_segments_];
+    memset(segment_offset_boundaries_, 0, sizeof(size_t) * num_segments_);
+
+    segment_sizes_ = new size_t[num_segments_];
+    memset(segment_sizes_, 0, sizeof(size_t) * num_segments_);
+
+  }
+
+  virtual ~InterpolationCorrelationIndex() {
+
+    delete[] segment_key_boundaries_;
+    segment_key_boundaries_ = nullptr;
+
+    delete[] segment_offset_boundaries_;
+    segment_offset_boundaries_ = nullptr;
+
+    delete[] segment_sizes_;
+    segment_sizes_ = nullptr;
+
+  }
+
+  virtual void lookup(const uint64_t key, std::vector<uint64_t> &ret_keys) final {
+
+    stats_.increment_find_op_counter();
+
+    if (this->size_ == 0) {
+      return;
+    }
+
+    if (key > key_max_ || key < key_min_) {
+      return;
+    }
+
+    // all keys are equal
+    if (key_min_ == key_max_) {
+      if (key_min_ == key) {
+        for (size_t i = 0; i < this->size_; ++i) {
+          ret_keys.push_back(this->container_[i].host_);
+        }
+      }
+      return;
+    }
+
+    // find suitable segment
+    size_t segment_id = (key - key_min_) / ((key_max_ - key_min_) / num_segments_);
+    if (segment_id > num_segments_ - 1) {
+      segment_id = num_segments_ - 1;
+    }
+
+    // the key should fall into: 
+    //  [ segment_key_boundaries_[i], segment_key_boundaries_[i + 1] ) -- if 0 <= i < num_segments_ - 1
+    //  [ segment_key_boundaries_[i], segment_key_boundaries_[i + 1] ] -- if i == num_segments_ - 1
+    if (segment_id < num_segments_ - 1) {
+
+      ASSERT(segment_key_boundaries_[segment_id] <= key, 
+        "beyond boundary: " << segment_key_boundaries_[segment_id] << " " << key);
+      ASSERT(key < segment_key_boundaries_[segment_id + 1], 
+        "beyond boundary: " << key << " " << segment_key_boundaries_[segment_id + 1]);
+
+    } else {
+
+      ASSERT(segment_id == num_segments_ - 1, 
+        "incorrect segment id: " << segment_id << " " << num_segments_ - 1);
+
+      ASSERT(segment_key_boundaries_[segment_id] <= key, 
+        "beyond boundary: " << segment_key_boundaries_[segment_id] << " " << key);
+      ASSERT(key <= segment_key_boundaries_[segment_id + 1], 
+        "beyond boundary: " << key << " " << segment_key_boundaries_[segment_id + 1]);
+    }
+
+    uint64_t segment_key_range = segment_key_boundaries_[segment_id + 1] - segment_key_boundaries_[segment_id];
+    
+    // guess where the data lives
+    int64_t guess = int64_t((key - segment_key_boundaries_[segment_id]) * 1.0 / segment_key_range * (segment_sizes_[segment_id] - 1) + segment_offset_boundaries_[segment_id]);
+
+    // TODO: workaround!!
+    if (guess >= this->size_) {
+      guess = this->size_ - 1;
+    }
+
+    int64_t origin_guess = guess;
+    
+    // if the guess is correct
+    if (this->container_[guess].target_ == key) {
+
+      stats_.measure_find_op_guess_distance(origin_guess, guess);
+
+      ret_keys.push_back(this->container_[guess].host_);
+      
+      // move left
+      int64_t guess_lhs = guess - 1;
+      while (guess_lhs >= 0) {
+
+        if (this->container_[guess_lhs].target_ == key) {
+          ret_keys.push_back(this->container_[guess_lhs].host_);
+          guess_lhs -= 1;
+        } else {
+          break;
+        }
+      }
+      // move right
+      int64_t guess_rhs = guess + 1;
+      while (guess_rhs <= this->size_ - 1) {
+
+        if (this->container_[guess_rhs].target_ == key) {
+          ret_keys.push_back(this->container_[guess_rhs].host_);
+          guess_rhs += 1;
+        } else {
+          break;
+        }
+      }
+    }
+    // if the guess is larger than the key
+    else if (this->container_[guess].target_ > key) {
+      // move left
+      guess -= 1;
+      while (guess >= 0) {
+
+        if (this->container_[guess].target_ < key) {
+          break;
+        }
+        else if (this->container_[guess].target_ > key) {
+          guess -= 1;
+          continue;
+        } 
+        else {
+
+          stats_.measure_find_op_guess_distance(origin_guess, guess);
+
+          ret_keys.push_back(this->container_[guess].host_);
+          guess -= 1;
+          continue;
+        }
+      }
+    } 
+    // if the guess is smaller than the key
+    else {
+      // move right
+      guess += 1;
+      while (guess < this->size_ - 1) {
+
+        if (this->container_[guess].target_ > key) {
+          break;
+        }
+        else if (this->container_[guess].target_ < key) {
+          guess += 1;
+          continue;
+        }
+        else {
+          
+          stats_.measure_find_op_guess_distance(origin_guess, guess);
+
+          ret_keys.push_back(this->container_[guess].host_);
+          guess += 1;
+          continue;
+        }
+      }
+    }
+    return;
+  }
+
+  virtual void lookup_range(const uint64_t &lhs_key, const uint64_t &rhs_key, std::vector<uint64_t> &ret_keys) final {
+
+    if (lhs_key > rhs_key) { return; }
+
+    if (lhs_key == rhs_key) {
+      lookup(lhs_key, ret_keys);
+      return;
+    }
+
+    if (this->size_ == 0) {
+      return;
+    }
+
+    if (lhs_key > key_max_ || rhs_key < key_min_) {
+      return;
+    }
+
+    // all keys are equal
+    if (key_min_ == key_max_) {
+      if (key_min_ >= lhs_key && key_min_ <= rhs_key) {
+        for (size_t i = 0; i < this->size_; ++i) {
+          ret_keys.push_back(this->container_[i].host_);
+        }
+      }
+      return;
+    }
+
+    int64_t lower_bound = find_lower_bound(lhs_key);
+    int64_t upper_bound = find_upper_bound(rhs_key);
+
+    for (size_t i = lower_bound; i <= upper_bound; ++i) {
+      ret_keys.push_back(this->container_[i].host_);
+    }
+    return;
+  }
+
+
   virtual void construct(std::vector<uint64_t> &target_column, std::vector<uint64_t> &host_column) final {
+
     assert(target_column.size() == host_column.size());
 
     size_ = target_column.size();
@@ -78,20 +307,207 @@ public:
 
     std::sort(container_, container_ + size_, compare_func);
 
+    key_min_ = this->container_[0].target_; // min value
+    key_max_ = this->container_[this->size_ - 1].target_; // max value
+
+    segment_key_boundaries_[0] = key_min_;
+    segment_key_boundaries_[num_segments_] = key_max_;
+
+    uint64_t key_range = key_max_ - key_min_;
+    uint64_t segment_key_range = key_range / num_segments_;
+
+    for (size_t i = 1; i < num_segments_; ++i) {
+      segment_key_boundaries_[i] = this->container_[0].target_ + segment_key_range * i;
+    }
+
+    size_t current_offset = 0;
+
+    segment_offset_boundaries_[0] = current_offset;
+
+    for (size_t i = 0; i < num_segments_ - 1; ++i) {
+      // scan the entire table to find offset boundaries
+      while (this->container_[current_offset].target_ < segment_key_boundaries_[i + 1]) {
+        ++segment_sizes_[i];
+        ++current_offset;
+      }
+      segment_offset_boundaries_[i + 1] = current_offset;
+    }
+
+    segment_sizes_[num_segments_ - 1] = this->size_ - current_offset;
+
   }
 
-  virtual void lookup(const uint64_t key, std::vector<uint64_t> &ret_keys) final {
-    for (size_t i = 0; i < size_; ++i) {
-      if (container_[i].target_ == key) {
-        ret_keys.push_back(container_[i].host_);
-      }
-      if (container_[i].target_ > key) {
-        return;
-      }
-    }
+  virtual void print() const final {
+
+    std::cout << "aggregated guess distance = " << stats_.find_op_guess_distance_ << std::endl;
+
+    std::cout << "number of profiled find operations = " << stats_.find_op_profile_count_ << std::endl;
+
+    std::cout << "average guess distance = " << stats_.find_op_guess_distance_ * 1.0 / stats_.find_op_profile_count_ << std::endl;
   }
+
+private:
+
+  int64_t find_lower_bound(const uint64_t &lower_key) {
+
+    ASSERT(lower_key <= key_max_, "lower_key must be <= key_max_");
+
+    if (lower_key <= key_min_) {
+      return 0;
+    }
+
+    size_t segment_id = (lower_key - key_min_) / ((key_max_ - key_min_) / num_segments_);
+    if (segment_id > num_segments_ - 1) {
+      segment_id = num_segments_ - 1;
+    }
+
+    // the lower_key should fall into: 
+    //  [ segment_key_boundaries_[i], segment_key_boundaries_[i + 1] ) -- if 0 <= i < num_segments_ - 1
+    //  [ segment_key_boundaries_[i], segment_key_boundaries_[i + 1] ] -- if i == num_segments_ - 1
+    if (segment_id < num_segments_ - 1) {
+
+      ASSERT(segment_key_boundaries_[segment_id] <= lower_key, 
+        "beyond boundary: " << segment_key_boundaries_[segment_id] << " " << lower_key);
+      ASSERT(lower_key < segment_key_boundaries_[segment_id + 1], 
+        "beyond boundary: " << lower_key << " " << segment_key_boundaries_[segment_id + 1]);
+
+    } else {
+
+      ASSERT(segment_id == num_segments_ - 1, 
+        "incorrect segment id: " << segment_id << " " << num_segments_ - 1);
+
+      ASSERT(segment_key_boundaries_[segment_id] <= lower_key, 
+        "beyond boundary: " << segment_key_boundaries_[segment_id] << " " << lower_key);
+      ASSERT(lower_key <= segment_key_boundaries_[segment_id + 1], 
+        "beyond boundary: " << lower_key << " " << segment_key_boundaries_[segment_id + 1]);
+    }
+
+    uint64_t segment_key_range = segment_key_boundaries_[segment_id + 1] - segment_key_boundaries_[segment_id];
+    // guess where the data lives
+    int64_t guess = int64_t((lower_key - segment_key_boundaries_[segment_id]) * 1.0 / segment_key_range * (segment_sizes_[segment_id] - 1) + segment_offset_boundaries_[segment_id]);
+
+    // TODO: workaround!!
+    if (guess >= this->size_) {
+      guess = this->size_ - 1;
+    }
+
+    if (this->container_[guess].target_ >= lower_key) {
+      // move left
+      while (guess - 1 >= 0) {
+        if (this->container_[guess - 1].target_ >= lower_key) {
+          --guess;
+        } else {
+          return guess;
+        }
+      }
+      return guess;
+
+    } else {
+      // move right
+      ++guess;
+      while (guess < this->size_) {
+        if (this->container_[guess].target_ < lower_key) {
+          ++guess;
+        } else {
+          return guess;
+        }
+      }
+      ASSERT(false, "shouldn't touch this line of code");
+      return guess;
+    }
+
+  }
+  
+  int64_t find_upper_bound(const uint64_t &upper_key) {
+    
+    ASSERT(upper_key >= key_min_, "upper_key must be >= key_min_");
+
+    if (upper_key >= key_max_) {
+      return this->size_ - 1;
+    }
+
+    size_t segment_id = (upper_key - key_min_) / ((key_max_ - key_min_) / num_segments_);
+    if (segment_id > num_segments_ - 1) {
+      segment_id = num_segments_ - 1;
+    }
+
+    // the upper_key should fall into: 
+    //  [ segment_key_boundaries_[i], segment_key_boundaries_[i + 1] ) -- if 0 <= i < num_segments_ - 1
+    //  [ segment_key_boundaries_[i], segment_key_boundaries_[i + 1] ] -- if i == num_segments_ - 1
+    if (segment_id < num_segments_ - 1) {
+
+      ASSERT(segment_key_boundaries_[segment_id] <= upper_key, 
+        "beyond boundary: " << segment_key_boundaries_[segment_id] << " " << upper_key);
+      ASSERT(upper_key < segment_key_boundaries_[segment_id + 1], 
+        "beyond boundary: " << upper_key << " " << segment_key_boundaries_[segment_id + 1]);
+
+    } else {
+
+      ASSERT(segment_id == num_segments_ - 1, 
+        "incorrect segment id: " << segment_id << " " << num_segments_ - 1);
+
+      ASSERT(segment_key_boundaries_[segment_id] <= upper_key, 
+        "beyond boundary: " << segment_key_boundaries_[segment_id] << " " << upper_key);
+      ASSERT(upper_key <= segment_key_boundaries_[segment_id + 1], 
+        "beyond boundary: " << upper_key << " " << segment_key_boundaries_[segment_id + 1]);
+    }
+
+    uint64_t segment_key_range = segment_key_boundaries_[segment_id + 1] - segment_key_boundaries_[segment_id];
+    // guess where the data lives
+    int64_t guess = int64_t((upper_key - segment_key_boundaries_[segment_id]) * 1.0 / segment_key_range * (segment_sizes_[segment_id] - 1) + segment_offset_boundaries_[segment_id]);
+
+    // TODO: workaround!!
+    if (guess >= this->size_) {
+      guess = this->size_ - 1;
+    }
+
+    if (this->container_[guess].target_ <= upper_key) {
+      // move right
+      while (guess +1 <= this->size_ - 1) {
+        if (this->container_[guess + 1].target_ <= upper_key) {
+          ++guess;
+        } else {
+          return guess;
+        }
+      }
+      return guess;
+
+    } else {
+      // move left
+      --guess;
+      while (guess > 0) {
+        if (this->container_[guess].target_ > upper_key) {
+          --guess;
+        } else {
+          return guess;
+        }
+      }
+      ASSERT(false, "shouldn't touch this line of code");
+      return guess;
+    }
+
+
+  }
+
+
+private:
 
   AttributePair *container_;
   size_t size_;
 
+  size_t num_segments_;
+
+  uint64_t key_min_;
+  uint64_t key_max_;
+
+  // there are num_segments_ + 1 key boundaries in total
+  uint64_t *segment_key_boundaries_; 
+
+  // there are num_segments_ offset boundaries in total
+  size_t *segment_offset_boundaries_;
+
+  // there are num_segments_ elements in segment_sizes_
+  size_t *segment_sizes_;
+
+  Stats stats_;
 };
--- correlation/correlation_test.cxx
+++ correlation/correlation_test.cxx
@@ -16,114 +16,148 @@
 #include "tuple_schema.h"
 
 
-typedef stx::btree_multimap<uint64_t, uint64_t> SecondaryIndex;
+class BTreeIndex {
+public:
 
-void primary_index_lookup(TupleSchema &tuple_schema, GenericDataTable *data_table, BaseGenericIndex *primary_index, const std::vector<uint64_t> &keys) {
+  BTreeIndex() {}
+  ~BTreeIndex() {}
+
+  void insert(const uint64_t key, const uint64_t value) {
+    container_.insert( {key, value} );
+  }
+
+  void lookup(const uint64_t key, std::vector<uint64_t> &ret_keys) {
+    auto ret = container_.equal_range(key);
+    for (auto it = ret.first; it != ret.second; ++it) {
+      ret_keys.push_back(it->second);
+    }    
+  }
+
+private:
+  stx::btree_multimap<uint64_t, uint64_t> container_;
+};
+
+uint64_t primary_index_lookup(TupleSchema &tuple_schema, GenericDataTable *data_table, BTreeIndex *primary_index, const std::vector<uint64_t> &keys) {
 
   size_t tuple_count = keys.size();
   size_t query_count = tuple_count;
 
+  uint64_t sum = 0;
+
   FastRandom rand_gen;
-  GenericKey query_key(sizeof(uint64_t));
   for (size_t query_id = 0; query_id < query_count; ++query_id) {
 
     uint64_t key = keys.at(rand_gen.next<uint64_t>() % tuple_count);
-    memcpy(query_key.raw(), (char*)(&key), sizeof(uint64_t));
 
-    std::vector<Uint64> offsets;
+    std::vector<uint64_t> offsets;
 
-    primary_index->find(query_key, offsets);
+    primary_index->lookup(key, offsets);
 
     for (auto offset : offsets) {
       char *value = data_table->get_tuple(offset);
-      size_t attr1_offset = tuple_schema.get_attr_offset(1);
-      size_t attr2_offset = tuple_schema.get_attr_offset(2);
-      uint64_t attr1_ret = *(uint64_t*)(value + attr1_offset);
-      uint64_t attr2_ret = *(uint64_t*)(value + attr2_offset);
-      std::cout << attr1_ret << " " << attr2_ret << std::endl;
+      size_t attr3_offset = tuple_schema.get_attr_offset(3);
+      uint64_t attr3_ret = *(uint64_t*)(value + attr3_offset);
+
+      sum += attr3_ret;
     }
-    std::cout << "===========" << std::endl;
   }
+
+  return sum;
 
 }
 
-void secondary_index_lookup(TupleSchema &tuple_schema, GenericDataTable *data_table, BaseGenericIndex *primary_index, SecondaryIndex *secondary_index, const std::vector<uint64_t> &keys) {
+uint64_t secondary_index_lookup(TupleSchema &tuple_schema, GenericDataTable *data_table, BTreeIndex *primary_index, BTreeIndex *secondary_index, const std::vector<uint64_t> &keys) {
 
   size_t tuple_count = keys.size();
   size_t query_count = tuple_count;
 
+  uint64_t sum = 0;
+
   FastRandom rand_gen;
-  GenericKey query_key(sizeof(uint64_t));
   for (size_t query_id = 0; query_id < query_count; ++query_id) {
 
     uint64_t key = keys.at(rand_gen.next<uint64_t>() % tuple_count);
-    auto ret = secondary_index->equal_range(key);
+
+    std::vector<uint64_t> pkeys;
+    
+    secondary_index->lookup(key, pkeys);
+    
     // found primary keys
-    for (auto iter = ret.first; iter != ret.second; ++iter) {
-      uint64_t pkey = iter->second;
-      memcpy(query_key.raw(), (char*)(&pkey), sizeof(uint64_t));
+    for (auto pkey : pkeys) {
 
-      std::vector<Uint64> offsets;
+      std::vector<uint64_t> offsets;
 
-      primary_index->find(query_key, offsets);
+      primary_index->lookup(pkey, offsets);
 
       for (auto offset : offsets) {
         char *value = data_table->get_tuple(offset);
-        size_t attr1_offset = tuple_schema.get_attr_offset(1);
-        size_t attr2_offset = tuple_schema.get_attr_offset(2);
-        uint64_t attr1_ret = *(uint64_t*)(value + attr1_offset);
-        uint64_t attr2_ret = *(uint64_t*)(value + attr2_offset);
-        std::cout << attr1_ret << " " << attr2_ret << std::endl;
+        size_t attr3_offset = tuple_schema.get_attr_offset(3);
+        uint64_t attr3_ret = *(uint64_t*)(value + attr3_offset);
+
+        sum += attr3_ret;
       }
-      std::cout << "===========" << std::endl;
-
     }
-
   }
+
+  return sum;
 
 }
 
-void correlation_index_lookup(TupleSchema &tuple_schema, GenericDataTable *data_table, BaseGenericIndex *primary_index, SecondaryIndex *secondary_index, BaseCorrelationIndex *correlation_index, const std::vector<uint64_t> &keys) {
+uint64_t correlation_index_lookup(TupleSchema &tuple_schema, GenericDataTable *data_table, BTreeIndex *primary_index, BTreeIndex *secondary_index, BaseCorrelationIndex *correlation_index, const std::vector<uint64_t> &keys) {
 
   size_t tuple_count = keys.size();
   size_t query_count = tuple_count;
 
+  uint64_t sum = 0;
+
   FastRandom rand_gen;
-  GenericKey query_key(sizeof(uint64_t));
   for (size_t query_id = 0; query_id < query_count; ++query_id) {
 
     uint64_t key = keys.at(rand_gen.next<uint64_t>() % tuple_count);
+
     std::vector<uint64_t> host_keys;
     // found host keys
     correlation_index->lookup(key, host_keys);
+
     for (auto host_key : host_keys) {
-      auto ret = secondary_index->equal_range(host_key);
+      std::vector<uint64_t> pkeys;
+
+      secondary_index->lookup(host_key, pkeys);
+
       // found primary keys
-      for (auto iter = ret.first; iter != ret.second; ++iter) {
-        uint64_t pkey = iter->second;
-        memcpy(query_key.raw(), (char*)(&pkey), sizeof(uint64_t));
+      for (auto pkey : pkeys) {
 
         std::vector<Uint64> offsets;
 
-        primary_index->find(query_key, offsets);
+        primary_index->lookup(pkey, offsets);
 
         for (auto offset : offsets) {
           char *value = data_table->get_tuple(offset);
-          size_t attr1_offset = tuple_schema.get_attr_offset(1);
+          
           size_t attr2_offset = tuple_schema.get_attr_offset(2);
-          uint64_t attr1_ret = *(uint64_t*)(value + attr1_offset);
           uint64_t attr2_ret = *(uint64_t*)(value + attr2_offset);
+
           if (attr2_ret == key) {
-            std::cout << attr1_ret << " " << attr2_ret << std::endl;
+            size_t attr3_offset = tuple_schema.get_attr_offset(3);
+            uint64_t attr3_ret = *(uint64_t*)(value + attr3_offset);
+
+            sum += attr3_ret;
           }
         }
-        std::cout << "===========" << std::endl;
       }
     }
   }
+
+  return sum;
 }
 
-void test() {
+enum AccessType {
+  PrimaryIndexAccess = 0,
+  SecondaryIndexAccess,
+  CorrelationIndexAccess
+};
+
+void test(const size_t tuple_count, const AccessType type, const size_t param) {
 
   TupleSchema tuple_schema;
   // add five uint64_t attributes
@@ -132,26 +166,17 @@ void test() {
   tuple_schema.add_attr(sizeof(uint64_t));
   tuple_schema.add_attr(sizeof(uint64_t));
 
-  IndexType index_type = IndexType::D_ST_StxBtree;
-
   size_t key_size = sizeof(uint64_t);
   size_t value_size = sizeof(uint64_t) * 3;
   std::unique_ptr<GenericDataTable> data_table(
     new GenericDataTable(key_size, value_size));
   // primary index
-  std::unique_ptr<BaseGenericIndex> primary_index(
-    create_generic_index(index_type, data_table.get()));
-
-  primary_index->prepare_threads(1);
-  primary_index->register_thread(0);
-
-  std::unique_ptr<SecondaryIndex> secondary_index(new SecondaryIndex());
+  std::unique_ptr<BTreeIndex> primary_index(new BTreeIndex());
+  std::unique_ptr<BTreeIndex> secondary_index(new BTreeIndex());
   std::unique_ptr<BaseCorrelationIndex> correlation_index(
-    new FullCorrelationIndex());
+    new InterpolationCorrelationIndex(param));
 
   FastRandom rand_gen;
-
-  size_t tuple_count = 1000;
 
   GenericKey tuple_key(key_size);
   GenericKey tuple_value(value_size);
@@ -164,7 +189,7 @@ void test() {
     uint64_t attr0 = rand_gen.next<uint64_t>(); // primary key
     uint64_t attr1 = tuple_id * tuple_id;
     uint64_t attr2 = tuple_id;
-    uint64_t attr3 = rand_gen.next<uint64_t>();
+    uint64_t attr3 = rand_gen.next<uint64_t>() % 100;
 
     attr0s.push_back(attr0);
     attr1s.push_back(attr1);
@@ -180,23 +205,51 @@ void test() {
     OffsetT offset = data_table->insert_tuple(tuple_key.raw(), tuple_key.size(), tuple_value.raw(), tuple_value.size());
 
     // update primary index
-    primary_index->insert(tuple_key, offset.raw_data());
+    primary_index->insert(attr0, offset.raw_data());
 
     // update secondary indexes
-    secondary_index->insert( {attr1, attr0} );
+    secondary_index->insert(attr1, attr0);
 
   }
 
   correlation_index->construct(attr2s, attr1s);
 
-  // primary_index_lookup(tuple_schema, data_table.get(), primary_index.get(), attr0s);
+  TimeMeasurer timer;
+  timer.tic();
 
-  // secondary_index_lookup(tuple_schema, data_table.get(), primary_index.get(), secondary_index.get(), attr1s);
+  uint64_t sum = 0;
+  switch (type) {
+    case PrimaryIndexAccess:
+    sum = primary_index_lookup(tuple_schema, data_table.get(), primary_index.get(), attr0s);
+    break;
+    case SecondaryIndexAccess:
+    sum = secondary_index_lookup(tuple_schema, data_table.get(), primary_index.get(), secondary_index.get(), attr1s);
+    break;
+    case CorrelationIndexAccess:
+    sum = correlation_index_lookup(tuple_schema, data_table.get(), primary_index.get(), secondary_index.get(), correlation_index.get(), attr2s);
+    break;
+    default:
+    break;
+  }
 
-  correlation_index_lookup(tuple_schema, data_table.get(), primary_index.get(), secondary_index.get(), correlation_index.get(), attr2s);
+  timer.toc();
+  std::cout << "elapsed time = " << timer.time_us() << " us" << std::endl;
+  std::cout << "sum = " << sum << std::endl;
 }
 
 
-int main() {
-  test();
+int main(int argc, char *argv[]) {
+  if (argc != 3 && argc != 4) {
+    std::cerr << "usage: " << argv[0] << " tuple_count access_type param" << std::endl;
+    return -1;
+  }
+
+  size_t tuple_count = atoi(argv[1]);
+  AccessType access_type = AccessType(atoi(argv[2]));
+  size_t param = 1;
+  if (argc == 4) {
+    param = atoi(argv[3]);
+  }
+
+  test(tuple_count, access_type, param);
 }
